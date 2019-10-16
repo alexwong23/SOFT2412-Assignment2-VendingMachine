f84589e68fe350fa803ad59e19a972d607d0c16d
Ken <kvoo2843@uni.sydney.edu.au>
2019-10-16 21:13:07 +1100

    remove and add to cart methods done

--- src/main/java/VendingMachine/model/ShoppingCart.java
+++ src/main/java/VendingMachine/model/ShoppingCart.java
@@ -7,16 +7,21 @@ import java.util.List;
 public class ShoppingCart {
 
     private List<Food> cart = new ArrayList<Food>(); // actual list of food.
-    private int itemCount;      // total number of items in the cart
+    private int cartSize;      // total number of items in the cart
     private double totalPrice;  // total price of items in the cart
 
     public ShoppingCart()
     {
-        itemCount = 0;
+        cartSize = 0;
 
         totalPrice = 0.0;
     }
 
+
+    /**
+     * Adds a Food item to the Cart.
+     * @param e
+     */
     public void addToCart(Food e)
     {
 
@@ -24,9 +29,26 @@ public class ShoppingCart {
 
         totalPrice = totalPrice + e.getPrice();
 
-        itemCount++;
+        cartSize++;
 
     }
+
+    /**
+     * Remove a Food item from the Cart.
+     * @param e
+     */
+    public void removeFromCart(Food e)
+    {
+
+        cart.remove(e);
+
+        totalPrice = totalPrice - e.getPrice();
+
+        cartSize--;
+
+    }
+
+
 
 
 }
