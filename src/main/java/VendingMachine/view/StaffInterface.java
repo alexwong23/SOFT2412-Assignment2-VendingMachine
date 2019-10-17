b1af1f5d0534af4d16f67e67665b1384e0fae7e4
Alex Wong <alexwongweilun@hotmail.co.uk>
2019-10-17 20:34:28 +1100

    created new branch staff by alex

--- src/main/java/VendingMachine/view/StaffInterface.java
+++ src/main/java/VendingMachine/view/StaffInterface.java
@@ -1,5 +1,25 @@
 package VendingMachine.view;
 
-public class StaffInterface implements CommandLineInterface {
+import VendingMachine.model.Food;
+import VendingMachine.model.VendingMachine;
 
+public class StaffInterface implements CommandLineInterface {
+	private VendingMachine vd;
+
+    public StaffInterface(VendingMachine vendingMachine) {
+    	this.vd = vendingMachine;
+        System.out.println("=========Welcome to vending machine!=========");
+        printFoodList();
+
+        // Code that reads staff input goes here
+        // Code that outputs data to staff goes here
+    }
+
+    public void printFoodList(){
+        System.out.printf("%-20s%-10s%-10s%-10s\n","Items"," Type","  Qua","Price");
+        System.out.println("---------------------------------------------");
+        for (Food item : this.vd.getAllFood()) {
+            System.out.println(item.getDisplayString());
+        }
+    }
 }
