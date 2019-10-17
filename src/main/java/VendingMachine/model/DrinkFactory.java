dce9b15b0e8dd7f0eb7b1d699498cc92ed2e386a
Kunxi Sun <ksun3708@uni.sydney.edu.au>
2019-10-17 21:35:15 +1100

    fix all food are displayed as Chips

--- src/main/java/VendingMachine/model/DrinkFactory.java
+++ src/main/java/VendingMachine/model/DrinkFactory.java
@@ -3,6 +3,6 @@ package VendingMachine.model;
 public class DrinkFactory implements FoodFactory {
     @Override
     public Food makeFood(String name, double price, int quantity) {
-        return new Chips(name, price, quantity);
+        return new Drink(name, price, quantity);
     }
 }
