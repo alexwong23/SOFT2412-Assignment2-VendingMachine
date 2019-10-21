4d53c1515bc000bbf7ecfd821f19ce334248eddf
phao5814 <patrick.hao95@gmail.com>
2019-10-22 10:52:22 +1100

    Added some comments

--- src/main/java/VendingMachine/model/Records.java
+++ src/main/java/VendingMachine/model/Records.java
@@ -1,4 +1,11 @@
 package VendingMachine.model;
 
 public class Records {
+    /* Transactions,
+    * date time / description / item / action /
+    *  */
+
+    /*
+    * Have a method for each type of transaction change. One for cancellation, another for purchase.
+    * */
 }
