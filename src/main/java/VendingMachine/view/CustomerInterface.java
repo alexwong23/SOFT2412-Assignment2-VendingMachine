d36017c7927dd15e38136366f400e169855e9433
ksun3708 <ksun3708@uni.syd.edu.au>
2019-11-05 10:48:41 +1100

    change currency name

--- src/main/java/VendingMachine/view/CustomerInterface.java
+++ src/main/java/VendingMachine/view/CustomerInterface.java
@@ -41,7 +41,6 @@ public class CustomerInterface implements CommandLineInterface {
         boolean shopping = true;
         while(shopping){
             printMainMenu();
-
             String input = scanner.next();
             switch (input){
                 case "1":
@@ -59,7 +58,6 @@ public class CustomerInterface implements CommandLineInterface {
                     notifier(input);
             }
         }
-        scanner.close();
     }
 
     public void purchaseInterface(){
@@ -195,10 +193,10 @@ public class CustomerInterface implements CommandLineInterface {
 
     public void printCurrencyList(){
         System.out.println("How would you like to pay?");
-        System.out.println("USD");
         System.out.println("AUD");
-        System.out.println("CNY");
-        System.out.println("JPY");
+        System.out.println("USD");
+        System.out.println("NZD");
+        System.out.println("SGD");
         System.out.println("CAD");
         System.out.println("Enter your selection: ");
     }
