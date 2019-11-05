72c887ddea3eb51d52056c09eaa5bdafea1dcf64
Oscar G <sgao0684@uni.sydney.edu.au>
2019-11-05 17:37:38 +1100

    created CustomerINterfacPaymentTest and created a test in it for a successful payment in AUD.

--- src/test/java/VendingMachine/view/CustomerInterfacePaymentTest.java
+++ src/test/java/VendingMachine/view/CustomerInterfacePaymentTest.java
@@ -1,4 +1,172 @@
+
 package VendingMachine.view;
 
+import VendingMachine.config.VendingMachineConfig;
+import VendingMachine.model.Drink;
+import VendingMachine.model.Inventory;
+import VendingMachine.model.InventoryItem;
+import VendingMachine.model.VendingMachine;
+import VendingMachine.ConfigReader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
 public class CustomerInterfacePaymentTest {
+
+    private VendingMachine vd;
+    private CustomerInterface ci;
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+    private ByteArrayInputStream inputStream;
+    private ByteArrayOutputStream outputStream;
+
+    String mainmenu = "===========Welcome to vending machine!================\n" +
+            "ID   Items               Type      Price     Quantity  \n" +
+            "------------------------------------------------------\n" +
+            "1    Pepsi               DRINK     5.0       10        \n" +
+            "2    Sprite              DRINK     5.0       10        \n" +
+            "3    Coke                DRINK     5.0       10        \n" +
+            "4    Orange Juice        DRINK     5.0       10        \n" +
+            "5    Red Rock Deli       CHIPS     5.0       10        \n" +
+            "6    Mars bar            CHOCOLATE 5.0       10        \n" +
+            "7    Jelly Beans         LOLLY     10.0      5         \n" +
+            "======================================================\n" +
+            "Options:\n" +
+            "1. Purchase\n" +
+            "2. Shopping Cart\n" +
+            "3. Quit\n" +
+            "Enter your options:\n";
+
+    @Before
+    public void setUp() throws Exception {
+        VendingMachineConfig vendingConfig = ConfigReader.readFoodNCashConfigs("src/test/resources/config.json");
+        vd = new VendingMachine(vendingConfig);
+        ci = new CustomerInterface(vd);
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void shoppingCartToPayment() {
+        InventoryItem test = vd.getInventory().getInventoryItemByFoodId(1);
+
+        ci.getShoppingCart().addToCart(test, 3);    //add 2 id 1 object into shopping cart
+
+        String in = "";
+        in += "2\n";      //Get into Shopping cart interface
+        in += "2\n";
+        in += "AUD\n";
+        in += "10\n";
+        in += "1\n";
+        in += "y\n";
+        in += "3\n";
+
+        setInput(in);
+
+        ci.run();
+
+        String out = "";
+        out += "Add successful.\n" +
+                "===========Welcome to vending machine!================\n" +
+                "ID   Items               Type      Price     Quantity  \n" +
+                "------------------------------------------------------\n" +
+                "1    Pepsi               DRINK     5.0       7         \n" +
+                "2    Sprite              DRINK     5.0       10        \n" +
+                "3    Coke                DRINK     5.0       10        \n" +
+                "4    Orange Juice        DRINK     5.0       10        \n" +
+                "5    Red Rock Deli       CHIPS     5.0       10        \n" +
+                "6    Mars bar            CHOCOLATE 5.0       10        \n" +
+                "7    Jelly Beans         LOLLY     10.0      5         \n" +
+                "======================================================\n" +
+                "Options:\n" +
+                "1. Purchase\n" +
+                "2. Shopping Cart\n" +
+                "3. Quit\n" +
+                "Enter your options:\n" +
+                "------------------Shopping Cart------------------\n" +
+                "ID   Items               Type      Price     Quantity  \n" +
+                "1    Pepsi               DRINK     5.0       3         \n" +
+                "Total Price: $ 15.0\n" +
+                "\n" +
+                "1. Delete Items\n" +
+                "2. Checkout\n" +
+                "How would you like to pay?\n" +
+                "AUD\n" +
+                "USD\n" +
+                "NZD\n" +
+                "SGD\n" +
+                "CAD\n" +
+                "Enter your selection: \n" +
+                "You need to pay: $15.00 in AUD\n" +
+                "You have paid: $0.00 in AUD\n" +
+                "ID   Items               Type      \n" +
+                "------------------------------------------------------\n" +
+                "1    10c                 COIN      0.1       10        \n" +
+                "2    20c                 COIN      0.2       10        \n" +
+                "3    50c                 COIN      0.5       10        \n" +
+                "4    $1                  COIN      1.0       10        \n" +
+                "5    $2                  COIN      2.0       10        \n" +
+                "6    $5                  NOTE      5.0       10        \n" +
+                "7    $10                 NOTE      10.0      10        \n" +
+                "8    $20                 NOTE      20.0      5         \n" +
+                "9    $50                 NOTE      50.0      2         \n" +
+                "10   $100                NOTE      100.0     1         \n" +
+                "======================================================\n" +
+                "\n" +
+                "Return to cart: 0 \tOR\n" +
+                "Enter ID: \n" +
+                "Enter Quantity:\n" +
+                "Cash inserted successfully.\n" +
+                "change is 85.0\n" +
+                "1 50.0 dollar note/coin was sent out\n" +
+                "1 20.0 dollar note/coin was sent out\n" +
+                "1 10.0 dollar note/coin was sent out\n" +
+                "1 5.0 dollar note/coin was sent out\n" +
+                "You have enough to checkout. Checkout now? (Y|N)\n" +
+                "Change of $85.00 in AUD received.\n" +
+                "Thank you for your purchase, come back again!\n" +
+                "Options:\n" +
+                "1. Purchase\n" +
+                "2. Shopping Cart\n" +
+                "3. Quit\n" +
+                "Enter your options:\n" +
+                "Thank you!\n";
+        assertEquals(out, getOutput());
+    }
+
+
+
+
+
+
+
+
+
+    private String getOutput() { // return OutputStream as a String
+        System.out.flush();
+        return outputStream.toString();
+    }
+
+    private void setInput(String userInput) { // set System.in to inputStream
+        inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+    }
+
+
+
+
 }
